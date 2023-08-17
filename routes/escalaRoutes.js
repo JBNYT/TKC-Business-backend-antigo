201747d96f6ad4c9315cd84f3174e147fe8d106d
Nalon <52545697+JBNYT@users.noreply.github.com>
2023-08-17 17:32:22 -0300

    Update escalaRoutes.js

--- routes/escalaRoutes.js
+++ routes/escalaRoutes.js
@@ -8,30 +8,45 @@ router.post("/", async (req, res) => {
   const { nome, email, telefone, resultados, idade, sexo } = req.body;
 
   try {
+    let escala;
+
     // Verifica se o email já existe no banco de dados
     const emailExistente = await Escala.findOne({ email });
 
     if (emailExistente) {
-      return res.status(400).json({ error: "Este email já está cadastrado." });
+      // Atualize o registro existente com os novos dados
+      emailExistente.nome = nome;
+      emailExistente.telefone = telefone;
+      emailExistente.resultados = resultados;
+      emailExistente.idade = idade;
+      emailExistente.sexo = sexo;
+
+      // Salve o registro atualizado
+      escala = await emailExistente.save();
+
+      res.status(200).json({
+        message: "Dados de escala de propósito atualizados com sucesso!",
+        escala,
+      });
+    } else {
+      // Crie um novo documento Escala com os dados recebidos
+      escala = new Escala({
+        nome,
+        email,
+        telefone,
+        resultados,
+        idade,
+        sexo,
+      });
+
+      // Salve o documento no banco de dados
+      const result = await escala.save();
+
+      res.status(201).json({
+        message: "Dados de escala de propósito salvos com sucesso!",
+        escala: result,
+      });
     }
-
-    // Crie um novo documento Predominancia com os dados recebidos
-    const escala = new Escala({
-      nome,
-      email,
-      telefone,
-      resultados,
-      idade,
-      sexo,
-    });
-
-    // Salve o documento no banco de dados
-    const result = await escala.save();
-
-    res.status(201).json({
-      message: "Dados de escala de proposito salvos com sucesso!",
-      predominancia: result,
-    });
   } catch (error) {
     res.status(500).json({
       error: error.message,
